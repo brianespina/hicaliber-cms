2de57353776a923186cf9beee43b2de6a3dfbaf2
espinabrian <espinabrian@gmail.com>
2023-08-24 20:52:36 +0800

    client add session function

--- app/dashboard/pages/edit/[id]/edit.js
+++ app/dashboard/pages/edit/[id]/edit.js
@@ -2,6 +2,8 @@
 import { useEffect, useState } from 'react'
 export default function EditForm({ formData }) {
     const [form, setForm] = useState(null)
+    const [sections, setSections] = useState([])
+
     useEffect(() => {
         setForm(formData)
     }, [formData])
@@ -15,13 +17,41 @@ export default function EditForm({ formData }) {
     }
     const handleSubmit = async (e) => {
         e.preventDefault()
+        console.log(form)
+    }
+
+    const sectionList = sections.map((section, idx) => {
+        return (
+            <div
+                className="p-8 mb-4 bg-gray-50 text-center rounded-md"
+                key={idx}
+            >
+                <h2 className="font-bold">{section.title}</h2>
+                {section.content}
+            </div>
+        )
+    })
+    const addSection = (e) => {
+        e.preventDefault()
+        setSections([
+            ...sections,
+            {
+                order: 1,
+                title: 'First Section',
+                content: 'this is the first section',
+            },
+        ])
     }
     return (
         <>
-            <form method="POST" action="/api/pages" className="md:w-6/12 mx-auto">
-                <div className='flex flex-col'>
+            <form
+                method="POST"
+                onSubmit={handleSubmit}
+                className="md:w-6/12 mx-auto"
+            >
+                <div className="flex flex-col">
                     <input
-                        className='form-control'
+                        className="form-control"
                         name="title"
                         placeholder="Page Title"
                         onChange={handleInputChange}
@@ -29,13 +59,13 @@ export default function EditForm({ formData }) {
                         value={form?.title || ''}
                     />
                     <textarea
-                        className='form-control'
+                        className="form-control"
                         name="content"
                         onChange={handleInputChange}
                         value={form?.content || ''}
                     ></textarea>
                     <input
-                        className='form-control'
+                        className="form-control"
                         name="slug"
                         placeholder="Page slug"
                         type="text"
@@ -43,9 +73,15 @@ export default function EditForm({ formData }) {
                         value={form?.slug || ''}
                     />
                 </div>
-                <button className="btn" type="submit" onClick={handleSubmit}>
-                    Create
-                </button>
+                <section className="mb-4">{sectionList}</section>
+                <div className="flex gap-2">
+                    <button className="btn" type="submit">
+                        Create
+                    </button>
+                    <button className="btn" onClick={addSection}>
+                        Add section +
+                    </button>
+                </div>
             </form>
         </>
     )
